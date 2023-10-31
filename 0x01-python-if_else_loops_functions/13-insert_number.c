d04a10263d1c84f1b1d6f70bed48792009e3c898
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-31 16:04:37 +0100

    Another go

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,33 +1,34 @@
 #include "lists.h"
-
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: head of the node
+ * @number: number to be inserted
+ * Return: the address of the new node, or NULL if it failed
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *fast = (*head)->next;
-	listint_t *slow = (*head);
-	listint_t *new;
+    listint_t *tmp = *head;
+    listint_t *new;
 
-	new = (listint_t *)malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
+    new = (listint_t *)malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
 
-	if (*head == NULL)
-	{
-		*head = new;
-		new->next = NULL;
-	}
-	else
-	{
-		while (fast != NULL)
-		{
-			if (fast->n > number)
-				break;
-			slow = slow->next;
-			fast = fast->next;
-		}
+    new->n = number;
+    new->next = NULL;
 
-		slow->next = new;
-		new->next = fast;
-	}
-	return (new);
+    if (*head == NULL || new->n < (*head)->n)
+    {
+        new->next = *head;
+        *head = new;
+        return (*head);
+    }
+
+    while (tmp->next != NULL && new->n > tmp->next->n)
+        tmp = tmp->next;
+
+    new->next = tmp->next;
+    tmp->next = new;
+
+    return (new);
 }
