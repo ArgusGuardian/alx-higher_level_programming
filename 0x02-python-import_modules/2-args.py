f19100186ebaf1fa8b0038e46e0fe289f5041118
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-02 15:08:33 +0100

    third task done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+len = len(sys.argv) - 1
+if len == 1:
+    print("1 argument:")
+elif len == 0:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(len))
+counter = 0
+for arg in sys.argv[1:]:
+    counter += 1
+    print("{}: {}".format(counter, arg))
