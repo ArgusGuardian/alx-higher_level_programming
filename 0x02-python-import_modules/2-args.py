1f7a4a5cfa8a49819efe9029f0c98be1825adaa5
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-02 15:14:53 +0100

    change variable name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
-len = len(sys.argv) - 1
-if len == 1:
+lenght = len(sys.argv) - 1
+if lenght == 1:
     print("1 argument:")
-elif len == 0:
+elif lenght == 0:
     print("0 arguments.")
 else:
-    print("{:d} arguments:".format(len))
+    print("{:d} arguments:".format(lenght))
 for i in range(1, len(sys.argv)):
     print("{:d}: {:s}".format(i, sys.argv[i]))
