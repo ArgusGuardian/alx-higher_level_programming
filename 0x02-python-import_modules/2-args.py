a5d0b310b786bf5d8294a5de9c2532c51955ba63
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-02 15:17:23 +0100

    condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-lenght = len(argv) - 1
-if lenght == 1:
-    print("1 argument:")
-elif lenght == 0:
-    print("0 arguments.")
-else:
-    print("{:d} arguments:".format(lenght))
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    lenght = len(argv) - 1
+    if lenght == 1:
+        print("1 argument:")
+    elif lenght == 0:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(lenght))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
