032ddecc01cf50267ff475b68836992cf4d1f651
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-04 15:53:15 +0100

    task seventh

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for grp in matrix:
+        alert = 0
+        for num in grp:
+            if alert == 0:
+                print("{:d}".format(num), end="")
+            else:
+                print(" {:d}".format(num), end="")
+            alert += 1
+        print()
