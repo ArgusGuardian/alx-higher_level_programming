25246ecf6f6ac6c30e2b493d71bf5453956042da
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-08 23:09:28 +0100

    easy

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    x = 0
+    factor = 1
+    for row in my_list:
+        for i in row:
+            factor *= i
+        x += factor
+        factor = 1
+    y = 0
+    for row in my_list:
+        y += row[-1]
+    return x / y
