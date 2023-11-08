c604f975fcf4f1eb5d9357566f8dd46833bb2135
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-08 21:29:36 +0100

    cleaner code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary:
         max = 0
+        max_key = None
         for key, value in a_dictionary.items():
-            if a_dictionary[key] > max:
-                max = a_dictionary[key]
+            if value > max:
+                max = value
                 max_key = key
         return max_key
     return None
