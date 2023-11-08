9a7bdb3bde1cda68ba41c6607878934604b822c8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-08 21:22:09 +0100

    task 11 done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        max = 0
+        for key, value in a_dictionary.items():
+            if a_dictionary[key] > max:
+                max = a_dictionary[key]
+                max_key = key
+        return max_key
+    return None
