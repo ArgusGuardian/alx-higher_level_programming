9cb8f72e547f3ccddf087b11d831da96159a3527
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-08 22:41:55 +0100

    handled all cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,20 @@
-#!/usr/bin/python3
 def roman_to_int(roman_string):
     number = 0
     i = 0
-    if roman_string:
-        while i < len(roman_string):
-            if i < len(roman_string) - 1 and roman_string[i] == 'I' and roman_string[i+1] in "MDCLXV":
-                number += roman_helper(roman_string[i+1]) - 1
-                i += 2
-            else:
-                number += roman_helper(roman_string[i])
-                i += 1
+    while i < len(roman_string):
+        if i < len(roman_string) - 1 and roman_string[i:i+2] in ["IV", "IX", "XC", "CD", "CM"]:
+            number += roman_helper(roman_string[i+1]) - \
+                roman_helper(roman_string[i])
+            i += 2
+        else:
+            number += roman_helper(roman_string[i])
+            i += 1
     return number
 
 
 def roman_helper(letter):
-    number = 0
-    if letter == 'M':
-        number += 1000
-    elif letter == 'D':
-        number += 500
-    elif letter == 'C':
-        number += 100
-    elif letter == 'L':
-        number += 50
-    elif letter == 'X':
-        number += 10
-    elif letter == 'V':
-        number += 5
-    elif letter == 'I':
-        number += 1
-    return number
+    doc = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for key, value in doc.items():
+        if key == letter:
+            return value
+    return 0
