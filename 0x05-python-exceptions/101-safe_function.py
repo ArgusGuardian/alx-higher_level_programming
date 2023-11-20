f55191ce8e90f34f92ec199e8e1cc9e90f67edb1
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-20 23:18:39 +0100

    second advanced task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        x = fct(*args)
+        return x
+    except Exception as e:
+        print(f"Excepton: {e}", file=sys.stderr)
+        return None
