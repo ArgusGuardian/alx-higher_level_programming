96ee7c4a8ec200f96d32f7c118a98a5e74a33ea5
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-20 21:24:55 +0100

    petty

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-            counter +=1
+            counter += 1
     except IndexError:
         pass
     print()
