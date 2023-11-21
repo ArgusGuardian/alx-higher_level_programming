9704b18e6a30ed479a32390256ff394871772b52
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 19:58:11 +0100

    task 2 done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" this is a file that wil have a Square class"""
+
+
+class Square:
+    """this block of code defines a class called Square"""
+    def __init__(self, size) -> None:
+        """init function of the class Square"""
+        self.__size = size
