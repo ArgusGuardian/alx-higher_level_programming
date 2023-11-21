f77913e97a2cec4097633daf01bf67726ccf09ba
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 21:16:32 +0100

    task 7 done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+"""this file will have a more advanced square class"""
+
+
+class Square:
+    """this is the square class"""
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        """init function for the class square"""
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
+
+    @property
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
