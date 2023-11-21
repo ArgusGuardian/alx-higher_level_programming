457ba1ffc15147c1ed3f707f7b3910b7648a3627
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 21:43:37 +0100

    added positon getter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,21 +10,36 @@ class Square:
         self.__size = size
         self.__position = position
 
+    def area(self):
+        return self.__size * self.__size
+
     @property
     def size(self):
-        return (self.__size)
+        """retrieve or set the size of the square"""
+        return self.__size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+        if value < 0:
+            raise TypeError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        if self.__size:
+            [print() for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print()
+        else:
+            print()
 
     @property
     def position(self):
-        return (self.__position)
+        """retrieve or set the size of the position"""
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -34,17 +49,3 @@ class Square:
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
-    def area(self):
-        return (self.__size * self.__size)
-
-    def my_print(self):
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
