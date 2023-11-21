506afe38dfc375a9ee1a601babf4bec3f3b59ca9
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 21:18:46 +0100

    weird

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,10 +26,11 @@ class Square:
 
     def my_print(self):
         if self.__size:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
         else:
             print()
 
