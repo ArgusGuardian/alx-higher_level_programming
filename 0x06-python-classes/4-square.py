aba731e1ff0d0a2e9ff8f4cd5dfeead9c078c03b
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 20:25:04 +0100

    task 5 done

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""this file will have a more advanced square class"""
+
+
+class Square:
+    """this is the square class"""
+    def __init__(self, size=0) -> None:
+        """init function for the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = value
