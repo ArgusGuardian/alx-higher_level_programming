d0e70021cf6f8635991983c3864d3e58fc896672
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 20:13:50 +0100

    task 3 done

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""this file will have a more advanced square class"""
+
+
+class Square:
+    """this is the square class"""
+    def __init__(self, size=0) -> None:
+        """init function for the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
