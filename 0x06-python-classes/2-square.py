fe2c866acba1338ac6df506a6135fe9f4d61c97c
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 20:08:35 +0100

    second task done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""this file will have a more advanced square class"""
+
+
+class Square:
+    """this is the square class"""
+    def __init__(self, size=0) -> None:
+        """init function for the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size=size
+
