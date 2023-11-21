258fbb455ff54a8721792bcdbc779cb63e2cf9f8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 19:49:44 +0100

    another go

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """a Square class that define a square"""
 
-
 class Square:
-    """empty class nothing to see here"""
+    """empty class defines class nothing to see here"""
     pass
