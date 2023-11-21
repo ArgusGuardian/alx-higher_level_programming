ec77e219fdbed776f437a84d357f98eef3c7df8e
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-21 22:03:41 +0100

    first advanced task done

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,1 +1,71 @@
 #!/usr/bin/python3
+"""create a linked list"""
+
+
+class Node:
+    """create a node"""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self._data = None
+        self._next_node = None
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """get data from linked list"""
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self._data = value
+
+    @property
+    def next_node(self):
+        """get next element of a node in linked list"""
+        return self._next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self._next_node = value
+
+
+class SinglyLinkedList:
+    """create a sorted linked list"""
+
+    def __init__(self):
+        """set head to none to begin with"""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """sort elements by data in increasing order"""
+        new_node = Node(value)
+
+        if self.head is None or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """print elements of list followed by a new line"""
+        result = []
+        current = self.head
+        while current:
+            result.append(current.data)
+            current = current.next_node
+        return '\n'.join(map(str, result))
