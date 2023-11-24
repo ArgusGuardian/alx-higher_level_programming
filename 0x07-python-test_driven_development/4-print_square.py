856f7a223be0e4fe8105d5befdb0bfb2213f8c54
Alaa Eddine <Alaaeddine@proton.me>
2023-11-24 17:00:32 -0500

    second commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Function that prints a square base on the var size"""
+
+
+def print_square(size):
+    """Print a square made of '#' characters
+
+    Args:
+        size: The size of the square
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+
+    Returns:
+        None
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
