a70f436961ba9c0a5de7f2d6525836bf9d025bdc
Alaa Eddine <Alaaeddine@proton.me>
2023-11-24 19:39:55 -0500

    task 5 done

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""Function that processes text and prints formatted lines"""
+
+
+def text_indentation(text):
+    """Process text and print formatted lines.
+
+    Args:
+        text: The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+
+    Returns:
+        None
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    cp_str = ""
+    for i in text:
+        alert = 0
+        cp_str += i
+        if i == '\n':
+            cp_str = cp_str.strip()
+            print(cp_str)
+            cp_str = ""
+
+        if i in ".?:":
+            print(f"{cp_str.strip()}\n")
+            cp_str = ""
+            alert = 1
+    if alert == 0 and not all(l == ' ' for l in cp_str):
+        print(cp_str.strip())
