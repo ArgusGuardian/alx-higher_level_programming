473cbad39d1d50867bb952bde0f4f7bfef1a5ee1
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 22:36:14 +0100

    100 done

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""defines int inhereted class"""
+
+
+class MyInt(int):
+    """class MyInt that inheretes from int"""
+    def __new__(cls, *args, **kwargs):
+        """rebel the versioin of integer"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """not is now is"""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """bypolar discussion"""
+        return int(self) == other
