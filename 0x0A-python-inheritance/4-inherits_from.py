4da3214ca73f547805f86c4bf2cdeb6831a7bb8f
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 21:41:22 +0100

    4 done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""define function to check inherited class"""
+
+
+def inherits_from(obj, a_class):
+    """function to check iniretance"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
