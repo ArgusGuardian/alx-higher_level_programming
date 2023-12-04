e8c331efa5638f1bad384ae1b056377ed0690cc8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 21:48:28 +0100

    6 done

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""dfines an empty class"""
+
+
+class BaseGeometry:
+    """this is an empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
