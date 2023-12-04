697f321be3bb313acbc370ef3843adda4db04b1d
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 21:35:24 +0100

    3 done

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""difines function to check object class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function to compare object class with class"""
+
+    return isinstance(obj, a_class)
