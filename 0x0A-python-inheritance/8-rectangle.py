1433cca7297e6343533fc85c96f555ff942e0e19
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 22:23:55 +0100

    8 done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""defines a rectangle class"""
+
+
+class Rectangle(BaseGeometry):
+    """class tha inherantes frmo basegeometry"""
+
+    def __init__(self, width, height):
+        """ initialize the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
