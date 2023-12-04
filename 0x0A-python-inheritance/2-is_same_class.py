2214ff753b4c7dd45e87b9e56d777d42187f59ee
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 21:30:30 +0100

    2 done

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""difines function to check object class"""
+
+
+def is_same_class(obj, a_class):
+    """function to compare object class with class"""
+    return isinstance(obj, a_class)
