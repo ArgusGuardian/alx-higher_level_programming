ee83d9195b1fb61a5cda3fb1797ecf61f4ea592d
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 21:05:22 +0100

    no floats

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """Mylist class inhereted list attributes"""
 
-    def __init__(self):
-        """init the onject"""
-        super().__init__()
-
     def print_sorted(self):
         """print sorted method"""
         print(sorted(self))
