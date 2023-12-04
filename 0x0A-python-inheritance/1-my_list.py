1b3477592c37dec7b378d6d7506d20670f284a20
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-04 21:00:48 +0100

    second commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Define a class mylist and function print sorted"""
+
+
+class MyList(list):
+    """Mylist class inhereted list attributes"""
+
+    def print_sorted(self):
+        """print sorted method"""
+        print(sorted(self))
