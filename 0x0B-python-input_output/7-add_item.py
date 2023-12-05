7426b68cb49685590df2b60b135bdc764e71645b
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 08:22:40 +0100

    8 done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """script tha laods from file appends then saves to json file"""
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
 try:
-    new = load_from_json_file('add_item.json')
+    new = load_from_json('add_item.json')
 except FileNotFoundError:
     new = []
 
-for element in argv:
+for element in argv[1:]:
     new.append(element)
 
-new = new[1:]
-
-save_to_json_file(new, 'add_item.json')
+save_to_json(new, 'add_item.json')
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""closs to json module"""
+
+
+def class_to_json(obj):
+    """function convert class to json"""
+    return obj.__dict__
