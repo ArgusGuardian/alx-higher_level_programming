69421d48f72a173ea684fd1ac733ee97c56bc083
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 07:41:39 +0100

    3 done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" function to serialize data to json format"""
+import json
+
+
+def to_json_string(my_obj):
+    """ function to serialize data to json format"""
+
+    return json.dumps(my_obj)
