aa7f09f30087fbe196328cd30ecc1f2b36a1c79a
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 14:44:46 +0100

    open with no flga

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """function loads from json file"""
-    with open(filename, 'w') as file:
+    with open(filename) as file:
         return json.load(file)
