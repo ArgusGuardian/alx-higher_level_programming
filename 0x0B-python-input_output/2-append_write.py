03854cdb9feef709e44a0aef41ad6e45411ee77a
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 14:34:45 +0100

    write instead of append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """function that appends to file and return number of chars"""
     with open(filename, 'w', encoding='utf-8') as file:
-        return file.append(text)
+        return file.write(text)
