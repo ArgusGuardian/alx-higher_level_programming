08f4f41d4b1486486ee622a1da26bceae7925aee
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 07:34:41 +0100

    2 done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""function that wirtes to file"""
+
+
+def write_file(filename="", text=""):
+    """function that writes to file and return number of chars"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""function that writes to file"""
+
+
+def append_write(filename="", text=""):
+    """function that appends to file and return number of chars"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.append(text)
