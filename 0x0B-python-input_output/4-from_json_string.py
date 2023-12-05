cbef68b5d574a517c943ee2045ddf3cb3001ff56
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 07:55:00 +0100

    5 done

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import json
+"""load from json format to py object"""
+
+
+def from_json_string(my_str):
+    """load from json format to py object"""
+
+    return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""function writes json to file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function writes json to file"""
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
