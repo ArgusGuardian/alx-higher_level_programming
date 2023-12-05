a3cce763c63c995a4f6dde9c7df1b768b3dc08d1
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 16:31:15 +0100

    100 done

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""module for the append function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that appends new_string to a line"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        line = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line.append(line)
+            if search_string in line:
+                line.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line)
