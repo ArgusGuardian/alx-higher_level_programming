7dfebdd731b3db41452ca01052dac53e4befc412
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 15:58:34 +0100

    pascal done

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-    
+    if n <= 0:
+        return [[]]
+    tri = [[1],]
+    for i in range(1, n):
+        row = [1]
+        for j in range(len(tri[i-1])):
+            if j == len(tri[i-1]) - 1:
+                row.append(1)
+            else:
+                row.append(tri[i-1][j] + tri[i-1][j+1])
+        tri.append(row)
+
+    return tri
