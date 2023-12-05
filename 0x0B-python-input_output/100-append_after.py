ecd7107ada0132f939c520e521d601dd86792b31
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-05 16:37:30 +0100

    file for f

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,14 +4,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """function that appends new_string to a line"""
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as file:
         line = []
         while True:
-            line = f.readline()
+            line = file.readline()
             if line == "":
                 break
             line.append(line)
             if search_string in line:
                 line.append(new_string)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(line)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.writelines(line)
