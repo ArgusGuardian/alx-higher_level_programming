2b2ccd35e44093e639cbc9d8e5b83320089bc5f5
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-09 21:26:47 +0100

    second commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,6 +91,7 @@ class Rectangle(Base):
             print()
 
     def update(self, *args, **kwargs):
+        """Funtion that updates the arguments"""
         self.__init__(self.width, self.height, self.x, self.y)
         if args and len(args) != 0:
             if len(args) >= 1:
@@ -118,20 +119,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Retrun the str() representation of the Rect class"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
-
-
-r1 = Rectangle(10, 10, 10, 10)
-print(r1)
-
-r1.update(height=1)
-print(r1)
-
-r1.update(width=1, x=2)
-print(r1)
-
-r1.update(y=1, width=2, x=3, id=89)
-print(r1)
-
-r1.update(x=1, height=2, y=3, width=4)
-print(r1)
+        return f"[Rectangle] ({self.id}) {self.x}/\
+{self.y} - {self.width}/{self.height}"
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,49 @@
 #!/usr/bin/python3
+"""Module for the Square class"""
+
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """function to initialize the Square instance"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter function fo size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter function for size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Funtion that updates the arguments"""
+        self.__init__(self.size, self.x, self.y)
+        if args and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs and len(kwargs) != 0:
+            for a, b in kwargs.items():
+                if a == "id":
+                    self.id = b
+                elif a == "size":
+                    self.size = b
+                elif a == "x":
+                    self.x = b
+                elif a == "y":
+                    self.y = b
+
+    def __str__(self):
+        """Retrun the str() representation of the Square class"""
+        return f"[Square] ({self.id}) {self.x}/\
+{self.y} - {self.size}"
