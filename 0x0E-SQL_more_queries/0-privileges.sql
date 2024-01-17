15c1fd561866b0c331498bc34116ea3ee3d6a1d5
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-17 15:37:50 +0100

    second commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list all privileges of 2 users on server
-SHOW GRANTS FOR user_0d_1@`localhost`, user_0d_2@`localhost`;
+SHOW GRANTS FOR user_0d_1@`localhost`;
+SHOW GRANTS FOE user_0d_2@`localhost`;
