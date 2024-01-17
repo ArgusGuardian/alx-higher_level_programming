d65781200b2c83c4f1c66ac7b9ecf028cf65ec5d
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-17 21:13:14 +0100

    Auto-commit changes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- show all the shows with the id of their genre
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id 
+SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows 
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
-ORDER BY tv_shows.title;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
