74c595f8aaf5e6a2814918bb38168fb5326fec85
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-17 21:25:01 +0100

    last modification

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,7 @@
 -- list all genres by their rating
-SELECT title, SUM(tv_show_ratings.rate) 'rating'
-FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY title
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
 ORDER BY rating DESC;
