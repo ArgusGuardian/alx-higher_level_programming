c72108c339caee7e909bf4415ae79aea8e52cb6e
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-17 17:08:27 +0100

    another go

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- list all the cities in the database
 USE hbtn_0d_usa;
-SELECT cities.id, cities.name, DISTINCT states.name
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
