c09be59adcdd51e3ff4734f407369dbc68e84f40
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-02-29 18:47:02 +0100

    seventh done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""find peak in unsorted list"""
+
+
+def find_peak(list_of_integers):
+    """find peak in unsorted list"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    count = 0
+    lentght = len(list_of_integers)
+    mid = ((lentght - count) // 2) + count
+    mid = int(mid)
+    if lentght == 1:
+        return list_of_integers[0]
+    if lentght == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
