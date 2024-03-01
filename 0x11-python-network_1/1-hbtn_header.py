2d3fe6bdd6177a0d1deb9a5b669fa1d311c58d66
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-03-02 00:14:37 +0100

    last done

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))!/usr/bin/python3
+        print(dict(response.headers).get("X-Request-Id"))
